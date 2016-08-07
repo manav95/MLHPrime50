6eafc1e4e0653f8362bb8b7a5122d281f5bb0b03
Mabb <mdutta7@gatech.edu>
2016-08-07 12:12:06 -0400

    Revisions

--- req.py
+++ req.py
@@ -7,16 +7,12 @@ import requests
 from PIL import Image, ImageOps,ImageEnhance, ImageFilter, ImageChops
 import pytesseract, os
 
-#with open("maturity.png","rb") as imagFile:
-#     f = imagFile.read()
-#     bytArray = bytearray(f)
-#     longString = cPickle.dumps(bytArray)
-image = Image.open("mainWindow.png")
+image = Image.open("daev.png")
 my_file = StringIO.StringIO()
 image.save(my_file, "PNG")
 my_file.seek(0)
 r = requests.post('http://127.0.0.1:5000/process',files={'imgFile':my_file})
-theText = r.text
+theText = r.text.encode('utf-8')
 theFil = open('myFile.txt', 'w')
 theFil.write(theText)
 theFil.close()
