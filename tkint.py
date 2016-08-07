70db8af37ef668e04c16faf05de61c7dbdedbf1a
Mabb <mdutta7@gatech.edu>
2016-08-07 12:36:52 -0400

    BackupCode

--- tkint.py
+++ tkint.py
@@ -1,8 +1,31 @@
 from Tkinter import *
-
+import json, io
+import PIL
+from tkFileDialog import askopenfilename
+from PIL import Image, ImageOps,ImageEnhance, ImageFilter, ImageChops
+import pytesseract, os
 class Application(Frame):
     def say_hi(self):
         print "hi there, everyone!"
+    def open_file_handler(self):
+        filePath = askopenfilename()
+        print filePath
+        return filePath
+
+    def upload(self):
+        filePath = self.open_file_handler()
+        image = Image.open(filePath)
+        image = image.filter(ImageFilter.DETAIL)
+        enhancer = ImageEnhance.Sharpness(image)
+        image = enhancer.enhance(2.0)
+        enhancer = ImageEnhance.Contrast(image)
+        image = enhancer.enhance(2.0)
+        #dpi = image.info["dpi"]
+        #if (dpi < 300):
+        #  image.info["dpi"] = 300
+        string = pytesseract.image_to_string(image)
+        print (string + "\n")
+        print ("\n\n\n\n\n")
 
     def createWidgets(self):
         self.QUIT = Button(self)
@@ -13,8 +36,8 @@ class Application(Frame):
         self.QUIT.pack({"side": "left"})
 
         self.hi_there = Button(self)
-        self.hi_there["text"] = "Hello",
-        self.hi_there["command"] = self.say_hi
+        self.hi_there["text"] = "Upload"
+        self.hi_there["command"] = self.upload
 
         self.hi_there.pack({"side": "left"})
 
